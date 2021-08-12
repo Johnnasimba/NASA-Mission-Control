4bcad1c676e3b6c33c4df76e6f9e2c2f4b791bcf
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-12 15:21:36 +0200

    Loading data on startup

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,5 +1,5 @@
 
-const planets = require('../../model/planets.module');
+const {planets} = require('../../model/planets.module');
 
 function getAllPlanets(req, res) {
    return res.status(200).json(planets);
--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,21 @@
 
 const http =  require('http')
+
 const app = require('./app')
+const PORT = process.env.PORT || 8000;
+
+const { loadPlanetsData } = require('./model/planets.module')
 
 const server =  http.createServer(app);
 
-const PORT = process.env.PORT || 8000;
+
+async function startServer() {
+    
+    await loadPlanetsData()
+    server.listen(PORT, () =>{
+        console.log(`Server listening on port ${PORT}`)
+    })
+}
 
 
-
-server.listen(PORT, () =>{
-    console.log(`Server listening on port ${PORT}`)
-})
-
-
+startServer();
