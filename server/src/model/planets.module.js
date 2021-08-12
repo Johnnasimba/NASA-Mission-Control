d66fbae7ca3295b7653ca387dca12945036b2b7a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-12 15:02:29 +0200

    Adding kepler data and creating its model

--- server/src/model/planets.module.js
+++ server/src/model/planets.module.js
@@ -1,3 +1,45 @@
-const planets = [];
+const parse = require('csv-parse');
 
-module.exports = planets;
+const fs = require('fs');
+
+const habitablePlanets = []
+const max_survivable_light = 1.11;
+const min_survivable_light = 0.36;
+const max_survivable_planet_radius = 1.6;
+
+
+function isHabitablePlanet(planet) {
+
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > min_survivable_light 
+    && planet['koi_insol'] < max_survivable_light
+    && planet['koi_prad'] < max_survivable_planet_radius
+    ;
+}
+
+    
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if(isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map(planet => {
+            return planet['kepler_name'];
+        }))
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        console.log('End');
+    });
+
+
+    module.exports = {
+        planets : habitablePlanets,
+    }
