79b5373f7973e12b6480550f35c27f145c2ec855
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-13 11:53:24 +0200

    Serving react front end in prodcution

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express =  require('express');
 const cors =  require('cors');
+const path = require('path')
 
 const planetRouter = require('./routes/planets/planets.router');
 
@@ -9,10 +10,11 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public', 'build')))
 app.use(planetRouter);
 
 app.get('/', (req, res) => {
-    res.send("Home page")
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
 module.exports = app
