1a02ca539640042423170decf7aa4385f7fe7907
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-16 15:19:08 +0200

    connecting to MongoDB with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,31 @@
 
 const http =  require('http')
-
+const mongoose =  require('mongoose');
 const app = require('./app')
+
+require('dotenv').config();
+
 const PORT = process.env.PORT || 8000;
 
 const { loadPlanetsData } = require('./model/planets.module')
 
 const server =  http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+});
 
-async function startServer() {
+mongoose.connection.on('error', (error) => {
+    console.error(error)
+})
+
+async function startServer() {    
+    await mongoose.connect(process.env.MONGO_URL,  {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
     
     await loadPlanetsData()
     server.listen(PORT, () =>{
