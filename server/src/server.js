708a61f42d126df4767dd8feabf4f9937ea3f99a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-18 10:39:16 +0200

    Updating test for mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,8 @@
 
-const http =  require('http')
-const mongoose =  require('mongoose');
-const app = require('./app')
+const http =  require('http');
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 
-require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,22 +10,9 @@ const { loadPlanetsData } = require('./model/planets.module')
 
 const server =  http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!')
-});
-
-mongoose.connection.on('error', (error) => {
-    console.error(error)
-})
 
 async function startServer() {    
-    await mongoose.connect(process.env.MONGO_URL,  {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-    
+    await mongoConnect();    
     await loadPlanetsData()
     server.listen(PORT, () =>{
         console.log(`Server listening on port ${PORT}`)
---
+++ server/src/services/mongo.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error(error)
+})
+
+const mongoURL = process.env.MONGO_URL
+
+async function mongoConnect() {
+    await mongoose.connect(mongoURL,  {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+}
+
+
+module.exports = {
+    mongoConnect,
+}
