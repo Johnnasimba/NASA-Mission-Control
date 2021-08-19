ded2385420a8974aa96d77935502d453c45fcaa0
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-19 15:38:14 +0200

    Loading SpaceX data

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const http =  require('http');
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./model/planets.module')
+const { loadLaunchData } = require('./model/launches.model');
 
 
 const PORT = process.env.PORT || 8000;
@@ -11,7 +12,8 @@ const server =  http.createServer(app);
 
 async function startServer() {    
     await mongoConnect();    
-    await loadPlanetsData()
+    await loadPlanetsData();
+    await loadLaunchData();
     server.listen(PORT, () =>{
         console.log(`Server listening on port ${PORT}`)
     })
