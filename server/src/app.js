d23eff5c9901f9571016e6756ec7c64b849d6ccf
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-19 10:22:04 +0200

    Versioning APIs

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,7 @@ const cors =  require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
-
+const api = require('./routes/api');
 const app =  express();
 
 app.use(cors({
@@ -16,8 +14,8 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public', 'build')))
-app.use('/planets', planetRouter);
-app.use('/launches', launchesRouter)
+
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'build', 'index.html'))
